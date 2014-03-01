ddf6a66ef09e27ab5eb73028949fd2de3899a421
frostpmr <marquis.palmer87@gmail.com>
2014-03-01 10:50:14 -0500

    new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TechSi::Application.config.secret_key_base = '2c4bd7396f068425989691ac46afc3384f0da0ceef5b0196a3d1cc708463065b178970650644d30637a64614693c82ca5a240f749c085ab7cdf57a016234efad'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TechSi::Application.config.session_store :cookie_store, key: '_TechSi_session'
